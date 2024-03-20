a7bae7e16e25016b4e16dc9cc4796be9b2705c87
José Ignacio Alegre <joseignacioalegre4@gmail.com>
2024-03-20 11:21:43 -0300

    Initial commit

---
+++ src/main/java/com/example/AHSAPI/AhsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.AHSAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AHSAPI/AhsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AHSAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

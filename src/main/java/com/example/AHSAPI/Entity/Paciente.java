4732fe84e3a9796da674f1236c6deceb1d2a97c1
José Ignacio Alegre <joseignacioalegre4@gmail.com>
2024-03-21 10:23:28 -0300

    Update Paciente.java

--- src/main/java/com/example/AHSAPI/Entity/Paciente.java
+++ src/main/java/com/example/AHSAPI/Entity/Paciente.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Entity;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Data
@@ -21,10 +22,33 @@ public class Paciente {
 
     @Id
     @GeneratedValue
-    private Long id;
-    private String name;
-    private String description;
-    private boolean archived;
-    private List<String> category;
-
+    private int idpaciente;
+    private int numhistclinica;
+    private String apellido;
+    private String nombre;
+    private int idtipodoc;
+    private int numerodocumento;
+    private String direccion;
+    private int idlocalidad;
+    private Timestamp fechanac;
+    private char sexo;
+    private String telefono;
+    private int idobrasocial;
+    private String nroafiliado;
+    private String nombrepadre;
+    private String nombremadre;
+    private int idusuario;
+    private Timestamp fechaalta;
+    private Timestamp fechamodif;
+    private char subidomps;
+    private String mail;
+    private int idpacinter3;
+    private String origen;
+    private char estado;
+    private boolean federado;
+    private String fechadef;
+    private String notadelpac;
+    private int idprointer3modif;
+    private int idestreferencia;
+    private String domicilioreal;
 }

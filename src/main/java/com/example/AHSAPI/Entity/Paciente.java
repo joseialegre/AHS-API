ddb60372c7c990f493d787533c6f8c35c6447e3e
José Ignacio Alegre <joseignacioalegre4@gmail.com>
2024-04-25 09:20:21 -0300

    refactor

--- src/main/java/com/example/AHSAPI/Entity/Paciente.java
+++ src/main/java/com/example/AHSAPI/Entity/Paciente.java
@@ -20,56 +20,17 @@ public class Paciente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "idpaciente")
     private int idpaciente;
-//    private int numhistclinica;
     private String apellido;
     private String nombre;
     private int idtipodoc; //3
     private int numerodocumento;
-//    private String direccion;
-//    private int idlocalidad;
     private Timestamp fechanac;
     private char sexo;
-//    private String telefono;
-//    private int idobrasocial;
-//    private String nroafiliado;
-//    private String nombrepadre;
-//    private String nombremadre;
-//    private int idusuario;
-//    private Timestamp fechaalta;
-//    private Timestamp fechamodif;
-//    private char subidomsp;
-//    private String mail;
-//    private int idpacinter3;
-//    private String origen;
-//    private char estado;
-//    private boolean federado;
-//    private String fechadef;
-//    private String notadelpac;
-//    private int idprointer3modif;
-//    private int idestreferencia;
-//    private String domicilioreal;
     private int numerotramite;
     private char ejemplar;
     private Timestamp fechaemision;
     private Timestamp fechavto;
 
-    public Paciente(int numerodocumento, String apellido, String nombre){
-        setNumerodocumento(numerodocumento);
-        setApellido(apellido);
-        setNombre(nombre);
-    }
 
-    public Paciente(int numerotramite,int numerodocumento, char ejemplar, String apellido, String nombre, Timestamp fechaemision, Timestamp fechavto, char sexo, Timestamp fechanac ){
-//        setNumerotramite(numerotramite);
-        setNumerodocumento(numerodocumento);
-//        setEjemplar(ejemplar);
-        setApellido(apellido);
-        setNombre(nombre);
-//        setFechaemision(fechaemision);
-//        setFechavto(fechavto);
-//        setIdtipodoc(3);
-//        setSexo(sexo);
-//        setFechanac(fechanac);
-    }
 
 }
